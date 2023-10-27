fe765e2534b6e072f29087e6756ba38e5bc55214
chan-shizu <bubbler018@gmail.com>
2023-10-28 00:35:47 +0900

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,153 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
+import Image from "next/image";
+import { useState, useEffect, useMemo } from "react";
+import {
+  collection,
+  getFirestore,
+  onSnapshot,
+  addDoc,
+} from "firebase/firestore";
+import { firebaseApp, TARGET_COLLECTION_NAME, db } from "../libs/firebase";
+
+const getCurrentTime = (date) => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    date.getFullYear() +
+    "/" +
+    ("0" + (date.getMonth() + 1)).slice(-2) +
+    "/" +
+    ("0" + date.getDate()).slice(-2) +
+    " " +
+    ("0" + date.getHours()).slice(-2) +
+    ":" +
+    ("0" + date.getMinutes()).slice(-2) +
+    ":" +
+    ("0" + date.getSeconds()).slice(-2)
+  );
+};
+export default function Home() {
+  const [text, setText] = useState();
+  const [currentUser, setCurrentUser] = useState("いけだ");
+  const [chatList, setChatList] = useState([]);
+
+  const onChangeText = (e) => {
+    setText(e.target.value);
+  };
+
+  const onChangeUser = (e) => {
+    setCurrentUser(e.target.value);
+  };
+
+  useEffect(() => {
+    let data;
+    let collectionMessages = [];
+    onSnapshot(collection(db, TARGET_COLLECTION_NAME), (snapshot) => {
+      collectionMessages = [];
+      snapshot.forEach((doc) => {
+        data = doc.data();
+        collectionMessages.push(data);
+      });
+      setChatList(collectionMessages);
+    });
+  }, []);
+
+  const submitText = async () => {
+    try {
+      const db = getFirestore(firebaseApp);
+      const col = collection(db, TARGET_COLLECTION_NAME);
+      await addDoc(col, {
+        user: currentUser,
+        text: text,
+        createdAt: new Date(),
+      });
+      setText("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const imagePath =
+    currentUser === "しずや"
+      ? "/person.jpg"
+      : currentUser === "いけだ"
+      ? "/person2.jpg"
+      : "/person3.jpg";
+
+  return (
+    <main className="bg-red-100 h-screen">
+      <section className="sticky top-0 h-14 flex justify-between w-full bg-red-100 items-center">
+        <div className="flex items-center">
+          <Image src="arrow.svg" width={40} height={40} />
+          <p className="font-semibold">山登り</p>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+        <div className="flex gap-3 pr-3">
+          <Image src="search.svg" width={20} height={20} />
+          <Image src="phone.svg" width={20} height={20} />
+          <Image src="memo.svg" width={20} height={20} />
+        </div>
+      </section>
+      <section className="pb-16 bg-red-100">
+        {chatList.map(({ user, text }, index) => (
+          <div
+            key={index}
+            className={`pl-3 pr-3 w-full ${
+              user === currentUser ? " text-right" : ""
+            }`}
+          >
+            <div
+              className={`flex mt-3 ${
+                user === currentUser ? " justify-end" : ""
+              }`}
+            >
+              <Image
+                className={`rounded-full ${
+                  user === currentUser ? " order-last ml-2" : "mr-2"
+                }`}
+                src={
+                  user === "しずや"
+                    ? "/person.jpg"
+                    : user === "いけだ"
+                    ? "/person2.jpg"
+                    : "/person3.jpg"
+                }
+                width={30}
+                height={30}
+              />
+              <p className="">{user}</p>
+            </div>
+            <p
+              className={`rounded-xl w-fit p-2 ml-5 break-words ${
+                user === currentUser
+                  ? "bg-sky-200 ml-auto mr-5"
+                  : "bg-white ml-5"
+              }`}
+            >
+              {text}
+            </p>
+          </div>
+        ))}
+      </section>
+      <section className="fixed h-14 w-full bottom-0 flex items-center justify-center gap-2 bg-white p-3">
+        <select onChange={onChangeUser}>
+          <option value="いけだ" checked={currentUser === "いけだ"}>
+            いけだ
+          </option>
+          <option value="しずや" checked={currentUser === "しずや"}>
+            しずや
+          </option>
+          <option value="まつの" checked={currentUser === "まつの"}>
+            まつの
+          </option>
+        </select>
+        <input
+          className="bg-slate-100 h-full rounded-full px-3"
+          value={text}
+          onChange={onChangeText}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <button onClick={submitText}>
+          <Image src="/send.png" width={20} height={20} />
+        </button>
+      </section>
     </main>
-  )
+  );
 }
